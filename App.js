114f057a9541f2279f0ae5509daca256f77cbc61
dhbriank <dhbriank@gmail.com>
2024-06-30 19:35:13 +0800

    text layout

--- App.js
+++ App.js
@@ -1,13 +1,53 @@
 import React from "react";
-import {View} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export default function App() {
   return (
-   <View style={{flex:1, flexDirection: "row"}}>
-    <View style={{flex:1, backgroundColor:"tomato"}}></View>
-    <View style={{flex:1, backgroundColor:"green"}}></View>
-    <View style={{flex:1, backgroundColor:"orange"}}></View>
-   </View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}> Kuala Lumpur </Text>
+      </View>
+      <View style = {styles.weather}>
+        <View style = {styles.day}>
+          <Text style= {styles.temp}>27</Text>
+          <Text style = {styles.description}>Sunny</Text>
+        </View>
+      </View>
+
+    </View>
+
   );
 }
 
+const styles = StyleSheet.create({
+  container:{
+    flex:1, 
+    backgroundColor:"tomato"
+  },
+  city:{
+    flex:1.2,
+    justifyContent:"center",
+    alignItems: "center",
+  },
+  cityName:{
+    color: "black",
+    fontSize: 30,
+    fontWeight: "500",
+  },
+  weather:{
+    flex:3,
+  },
+  day:{
+    flex:1,
+    alignItems: "center",
+
+  },
+  temp:{
+    marginTop: 50,
+    fontSize: 168,
+  },
+  description:{
+    marginTop: -20,
+    fontSize: 40,
+  }
+})
